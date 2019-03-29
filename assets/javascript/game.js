923f8ffd12f457f369d4d6af6ae1601a31683423
Louis Rodriguez <louisp285@yahoo.com>
2019-03-29 01:08:43 -0500

    updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+// need random box to begins with a number//
+//set computer random choice between the specified figures//
+//wins and loses need to begin at zero//
+//crystals need to be assigned a function to derive random numbers//
+//the numbers for the computer are tallied in the rplayers score box//
+//when player either hit the correct number, add win or add loss//
