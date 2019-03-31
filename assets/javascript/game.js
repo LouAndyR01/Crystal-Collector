88c88381252bd130f1f293e68c3ba3e851ca9836
Louis Rodriguez <louisp285@yahoo.com>
2019-03-30 20:57:50 -0500

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,14 @@
-// need random box to begin with a number, generate number to begin the game
-// set computer random choice between the specified figures
-// wins and loses need to begin at zero
-// crystals need to be assigned a function to derive random numbers
-// the numbers for the computer are tallied in the players score box
-// when player either hits the correct number, add win or add loss
-
-// game with 4 crystals. every crystal needs to have a random number. 
-// number should be generated every single time a player wins or loses. 
-// when clicking on any crystal, there should be a adding with a result 
-// until it hits the total score or goes over.
-// Every crystal is to have a random number from 1-12.
-// if it equals, then the win counter goes up or the losing count goes up.
+// set need global variables to be able to start the game.
+// set computer random choice between the specified figure 19-120.
+// wins and loses need to begin at zero to begin the game.
+// crystals need to be assigned a function to derive random numbers.
+// the numbers for the computer need to be tallied in the random score box.
+// when player either hits the correct number, or goes over the random number, add win or add loss.
+// when clicking on any crystal, there should be adding each number as a result of the previous .on click event.
+// set each crystal to generate a random number from 1-12.
+// make sure the HTML is updated - .text.
+// creat alert for winner or loser.
+// BE SURE TO CONSOLE.LOG!
 
     // globals to start the game.
     var wins = 0;
@@ -27,7 +25,7 @@
     // this function will operate when the game starts.
 function gameStart() {
     answer = Math.floor(Math.random() * 101) + 19;
-    $("#random-number").html(answer);
+    $("#random-number").text(answer);
     // console.log(answer);
 
     green_button = Math.floor(Math.random() * 12) + 1;
@@ -43,41 +41,41 @@ function gameStart() {
 
     // on click functions for each button.
 $("#green_crystal").on("click", function () {
-    $("#green_crystal").html(green_button);
+    $("#green_crystal").text(green_button);
     yourScore += green_button;
-    $("#your-score").html(yourScore);
+    $("#your-score").text(yourScore);
     // console.log(green_button);
     // console.log(yourScore);
 });
 $("#yellow_crystal").on("click", function () {
-    $("#yellow_crystal").html(yellow_button);
+    $("#yellow_crystal").text(yellow_button);
     yourScore += yellow_button;
-    $("#your-score").html(yourScore);
+    $("#your-score").text(yourScore);
     
 });
 $("#blue_crystal").on("click", function () {
-    $("#blue_crystal").html(blue_button);
+    $("#blue_crystal").text(blue_button);
     yourScore += blue_button;
-    $("#your-score").html(yourScore);
+    $("#your-score").text(yourScore);
    
 });
 $("#purple_crystal").on("click", function () {
-    $("#purple_crystal").html(purple_button);
+    $("#purple_crystal").text(purple_button);
     yourScore += purple_button;
-    $("#your-score").html(yourScore);
+    $("#your-score").text(yourScore);
    
 });
     // Checking to see if it is a winner or loser here.
 $(".images").on("click", function () {
     if (yourScore === answer) {
         wins++;
-        $("#wins").html(wins);
+        $("#wins").text(wins);
         alert("You Win!");
         gameStart();
     }
     else if (yourScore > answer) {
         losses++;
-        $("#losses").html(losses);
+        $("#losses").text(losses);
         alert("You lose!");
         gameStart();
     }
